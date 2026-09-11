41f82288e328b8b91e5ff43fa2b9e773855f8652
Luke Carrier <luke@carrier.family>
2026-09-11 07:22:09 +0100

    flake: bump nixpkgs to 26.05

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-26.05";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,7 @@
 final: prev:
 let
   fetchCargoVendor = import "${nixpkgs.outPath}/pkgs/build-support/rust/fetch-cargo-vendor.nix" {
-    inherit (final) lib stdenvNoCC runCommand writers python3Packages cargo cacert;
+    inherit (final) lib stdenvNoCC runCommand writers python3Packages cargo gitMinimal cacert;
     nix-prefetch-git = final.nix-prefetch-git.override { git-lfs = null; };
   };
 in
